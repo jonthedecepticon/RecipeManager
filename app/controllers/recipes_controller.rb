314a4e362d8852cd63a45774904ddc4a674baba2
Jon Lambson <jon@crowdengine.com>
2016-09-19 22:46:05 -0600

    added css and new recipe method

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,5 +3,27 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
   end
 
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
 
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+
+    if @recipe.save
+      flash[:success] = "Your recipe was saved succesfully"
+      redirect_to recipes_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def recipe_params
+      params.require(:recipe).permit(:name, :summary, :description)
+    end
 end
