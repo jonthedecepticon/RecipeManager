2a4c7fa9016b45c166c407061ab1188a57b982fc
Jon Lambson <jon@crowdengine.com>
2016-09-21 22:40:30 -0600

    added update/edit action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,11 @@
 class RecipesController < ApplicationController
+  before_action :find_recipe, only: [:show, :create, :edit, :update]
+
   def index
     @recipes = Recipe.all
   end
 
   def show
-    @recipe = Recipe.find(params[:id])
   end
 
   def new
@@ -13,7 +14,6 @@ class RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.new(recipe_params)
-
     if @recipe.save
       flash[:success] = "Your recipe was saved succesfully"
       redirect_to recipes_path
@@ -22,7 +22,23 @@ class RecipesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      flash[:success] = "Your recipe was succesfully updated"
+      redirect_to recipe_path(@recipe)
+    else
+      render 'edit'
+    end
+  end
+
   private
+    def find_recipe
+      @recipe = Recipe.find(params[:id])
+    end
+
     def recipe_params
       params.require(:recipe).permit(:name, :summary, :description)
     end
