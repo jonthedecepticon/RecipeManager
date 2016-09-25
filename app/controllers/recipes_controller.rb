f548bef218032bdc724261b3ad1ac8392c1d585a
Jon Lambson <jon@crowdengine.com>
2016-09-24 23:31:37 -0600

    Added like feature
    :

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,8 @@
 class RecipesController < ApplicationController
-  before_action :find_recipe, only: [:show, :edit, :update]
+  before_action :find_recipe, only: [:show, :edit, :update, :like]
 
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.paginate(page: params[:page], per_page: 2)
   end
 
   def show
@@ -33,6 +33,16 @@ class RecipesController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def like
+    like = Like.create(like: params[:like], chef: Chef.first, recipe: @recipe)
+    if like.valid?
+      flash[:success] = "You have liked this recipe!"
+    else
+      flash[:danger] = "Sorry, only one vote per recipe!"
+    end
+    redirect_to :back
   end
 
   private
