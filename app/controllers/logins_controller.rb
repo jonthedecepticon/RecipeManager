7bdd6462ff716ed6e553fd6217c9bee74a74c197
Jon Lambson <jon@crowdengine.com>
2016-10-03 08:47:06 -0600

    adding some gaurds

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -6,7 +6,7 @@ class LoginsController < ApplicationController
     chef = Chef.find_by(email: params[:email])
     if chef && chef.authenticate(params[:password])
       session[:chef_id] = chef.id
-      flash[:success] = "Currently logged in"
+      flash[:success] = "Successfully signed in. Welcome #{chef.chefname}"
       redirect_to recipes_path
     else
       flash.now[:danger] = "Incorrect email or password."
@@ -16,7 +16,7 @@ class LoginsController < ApplicationController
 
   def destroy
     session[:chef_id] = nil
-    flash[:success] = "You are logged out"
+    flash[:success] = "You have successfully been signed out."
     redirect_to root_path
   end
 
